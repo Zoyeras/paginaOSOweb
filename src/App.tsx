87963165cfa79b5710fdf753c63b3178eb7a2161
zoyeras <loaizaocampos@gmail.com>
2025-08-03 17:22:07 -0500

    pagina home

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
+import Home from "./paginas/homePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
